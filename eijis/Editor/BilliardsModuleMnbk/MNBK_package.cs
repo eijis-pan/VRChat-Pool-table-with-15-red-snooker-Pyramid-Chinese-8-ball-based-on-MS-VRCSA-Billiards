73c5295fba5488fdd8daa66725ad6e07890e189e
eijis-pan <eijis@to2488.ddns.net>
2025-05-08 21:05:56 +0900

    CBC1.391用みんボコ3.0.6a

--- eijis/Editor/BilliardsModuleMnbk/MNBK_package.cs
+++ eijis/Editor/BilliardsModuleMnbk/MNBK_package.cs
@@ -8,7 +8,7 @@ namespace EijisMsVrcsaTableUtil
 {
 	public class Package
 	{
-		private static readonly string exportPackageFilePath = "CBCTable1391_mnbk_306.unityPackage";
+		private static readonly string exportPackageFilePath = "CBCTable1391_mnbk_306a.unityPackage";
 		static readonly string[] exportFilePaths = 
 		{
 			//"Assets/eijis/Editor/MsVrcsaTableUtil/MNBK_table_setup.cs",
@@ -55,7 +55,7 @@ namespace EijisMsVrcsaTableUtil
 			"Assets/VRChat-Pool-table-15-red-snooker-Pyramid-Chinese-8-MS-VRCSA-Billiards/Cheese/Translate/TextJson/en.Json",
 			"Assets/VRChat-Pool-table-15-red-snooker-Pyramid-Chinese-8-MS-VRCSA-Billiards/Cheese/Translate/TextJson/ja.Json",
 			"Assets/VRChat-Pool-table-15-red-snooker-Pyramid-Chinese-8-MS-VRCSA-Billiards/Cheese/Translate/TextJson/zh.Json",
-			// "Assets/VRChat-Pool-table-15-red-snooker-Pyramid-Chinese-8-MS-VRCSA-Billiards/Prefab/old/snooker&pyramid&cn8&3c.prefab",
+			"Assets/VRChat-Pool-table-15-red-snooker-Pyramid-Chinese-8-MS-VRCSA-Billiards/Prefab/old/snooker&pyramid&cn8&3c.prefab",
 			"Assets/VRChat-Pool-table-15-red-snooker-Pyramid-Chinese-8-MS-VRCSA-Billiards/Prefab/snooker&pyramid&cn8&3c&10b.prefab",
 			// "Assets/VRChat-Pool-table-15-red-snooker-Pyramid-Chinese-8-MS-VRCSA-Billiards/Modules/BilliardsModule/ht8b_materials/procedural/tballs_3Cushion_YellowCueBall.png",
 			"Assets/VRChat-Pool-table-15-red-snooker-Pyramid-Chinese-8-MS-VRCSA-Billiards/Modules/BilliardsModule/Materials/SkipTurnButton.mat",
@@ -93,7 +93,7 @@ namespace EijisMsVrcsaTableUtil
 			"Assets/VRChat-Pool-table-15-red-snooker-Pyramid-Chinese-8-MS-VRCSA-Billiards/eijis/UdonScripts/ScoreScreen/TeamPlayers.cs"
 		};
 		
-		[MenuItem("GameObject/MNBK/ExportPackage306", false, 0)]
+		[MenuItem("GameObject/MNBK/ExportPackage306a", false, 0)]
 		private static void ExportPackage_Menu(MenuCommand command)
 		{
 			try
